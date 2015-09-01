8c2b136416657517958e1680eb1b2ab3a4b277f3
fumes <fumesmag@gmail.com>
2015-09-01 11:38:07 +0200

    .

--- _js/globalo.js
+++ _js/globalo.js
@@ -1,15 +1,1 @@
-/** 2 BLAH theme Functionality*/
-
-//fade other when hover on element
-// jQuery(document).ready(function($) {
-//     // Inside of this function, $() will work as an alias for jQuery()
-//     // and other libraries also using $ will not be accessible under this shortcut
-
-// 		$('.blah_blox_wrap').hover(function() {
-// 		    $(this).addClass('hovered');
-// 		    $('.blah_blox_wrap').not('.hovered').addClass('nothovered');
-// 		}, function() {
-// 		    $('.blah_blox_wrap').removeClass('nothovered hovered');
-// 		});
-// });
 
