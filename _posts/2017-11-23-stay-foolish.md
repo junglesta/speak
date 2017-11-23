240cbe53e6811a634684113574caff76fdb5e590
toybreaker <rokmabox@gmail.com>
2017-11-23 10:36:41 +0800

    Stay hungry. Stay foolish.

---
+++ _posts/2017-11-23-stay-foolish.md
@@ -0,0 +1,10 @@
+---
+layout: post
+title: "stay foolish"
+categories: english advice statement
+author: Stewart Brand
+goto: https://en.wikipedia.org/wiki/Stewart_Brand?ref=speak.junglestar.org
+go: think
+gocheck:
+---
+Stay hungry. Stay foolish.
--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var webshot = require("gulp-webshot");
 gulp.task("webshotPNG", function() {
   // return gulp.src('./_site/**/*.html') //all
   return gulp
-    .src("./_site/pill-make-you-sick/*.html") //specific page
+    .src("./_site/stay-foolish/*.html") //specific page
     .pipe(
       webshot({
         dest: "./assets/png-screenshots/",
