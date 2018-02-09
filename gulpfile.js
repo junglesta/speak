465111ffdbd7faeaa14e6069bfc7a83f22729bc8
toybreaker <rokmabox@gmail.com>
2018-02-09 15:56:30 +0100

    need to fuck

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var webshot = require("gulp-webshot");
 gulp.task("webshotPNG", function() {
   // return gulp.src('./_site/**/*.html') //all
   return gulp
-    .src("./_site/nothing-is-forever/*.html") //specific page
+    .src("./_site/need-to-fuck/*.html") //specific page
     .pipe(
       webshot({
         dest: "./assets/png-screenshots/",
